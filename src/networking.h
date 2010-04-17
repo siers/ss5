e96bc347bf4d773b4c2bce86e9e7cadc1621d174
siers <wimuan@gmail.com>
2010-04-17 13:52:03 +0300

    fuse_sockets() fixed

--- src/networking.h
+++ src/networking.h
@@ -12,6 +12,7 @@ char resolvehost(struct sockaddr_in*, char*);
 int myconnect_ip(uint32_t, uint16_t, int*);
 int myconnect_domain(char*, uint16_t, int*, uint32_t*);
 int create_socket(uint16_t alterport);
+void cat_socket(int);
 void fuse_sockets(int, int, s_client*);
 sig_atomic_t accept_loop(int fd);
 
